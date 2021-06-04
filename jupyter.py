5f89a29ea1aa4dbd0c7a04b1410dafe85bd1240a
shibinmak <m.shibin96@gmail.com>
2021-06-04 23:57:26 +0530

    pass change

--- jupyter.py
+++ jupyter.py
@@ -10,7 +10,7 @@ c.NotebookApp.allow_remote_access = True
 c.NotebookApp.open_browser = False
 # ipython -c "from notebook.auth import passwd; passwd()"
 c.NotebookApp.password =  'argon2:$argon2id$v=19$m=10240,t=10,p=8$vtM02Ja+JWO7M2Wyt6Pv9A$pwEKNjpf1Tpu5x04c8fCjw'
-c.NotebookApp.port = int(8888)
+c.NotebookApp.port = int(17061)
 c.NotebookApp.allow_root = True
 c.NotebookApp.allow_password_change = True
 c.ConfigurableHTTPProxy.command = ['configurable-http-proxy', '--redirect-port', '80']
